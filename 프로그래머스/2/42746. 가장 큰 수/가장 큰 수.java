6dcd09f0ab45a915930840b6532cbcb728a842f8
JISOO:)👩‍💻 <155516153+elin0601@users.noreply.github.com>
2024-07-02 14:50:11 +0900

    [level 2] Title: 가장 큰 수, Time: 1.74 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,29 +1,29 @@
-import java.util.Arrays;
-import java.util.Comparator;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
     public String solution(int[] numbers) {
-        // int 배열을 String 배열로 변환
-        String[] strNumbers = Arrays.stream(numbers)
-                                    .mapToObj(String::valueOf)
-                                    .toArray(String[]::new);
+        String answer = "";
 
-        // 문자열 배열을 정렬하는데, x+y가 y+x보다 큰지 비교하는 커스터마이즈된 Comparator 사용
-        Arrays.sort(strNumbers, new Comparator<String>() {
-            @Override
-            public int compare(String x, String y) {
-                return (y + x).compareTo(x + y);  // 내림차순으로 정렬
-            }
-        });
-
-        // 배열을 문자열로 합칩니다.
-        StringBuilder sb = new StringBuilder();
-        for (String num : strNumbers) {
-            sb.append(num);
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < numbers.length; i++) {
+            list.add(numbers[i]);
         }
-
-        // "0"으로만 이루어진 경우를 처리
-        String result = sb.toString();
-        return result.charAt(0) == '0' ? "0" : result;
+        Collections.sort(list, (a, b) -> {
+            String as = String.valueOf(a), bs = String.valueOf(b);
+            return -Integer.compare(Integer.parseInt(as + bs), Integer.parseInt(bs + as));
+        });
+        StringBuilder sb = new StringBuilder();
+        for(Integer i : list) {
+            sb.append(i);
+        }
+        answer = sb.toString();
+        if(answer.charAt(0) == '0') {
+            return "0";
+        }else {
+            return answer;
+        }
     }
 }
