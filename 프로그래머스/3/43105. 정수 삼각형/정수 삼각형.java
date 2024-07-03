612df9db37f2e8a8c2e36aa32af4a1d93550ab84
JISOO:)👩‍💻 <155516153+elin0601@users.noreply.github.com>
2024-07-03 14:36:57 +0900

    [level 3] Title: 정수 삼각형, Time: 7.31 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,16 +1,13 @@
+import java.util.*;
 class Solution {
     public int solution(int[][] triangle) {
-      int height = triangle.length;
-        
-        // triangle 배열을 밑에서부터 갱신
-        for (int i = height - 2; i >= 0; i--) {
-            for (int j = 0; j <= i; j++) {
-                // 아래 층의 두 값 중 더 큰 값을 선택해서 현재 값을 갱신
-                triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
-            }
+        for (int i = 1; i < triangle.length; i++) {
+            triangle[i][0] += triangle[i-1][0];
+            triangle[i][i] += triangle[i-1][i-1];
+            for (int j = 1; j < i; j++) 
+                triangle[i][j] += Math.max(triangle[i-1][j-1], triangle[i-1][j]);
         }
-        
-        // 꼭대기의 값이 최대 경로 합
-        return triangle[0][0];
+
+        return Arrays.stream(triangle[triangle.length-1]).max().getAsInt();
     }
 }
