6ec1e30c03876e70333c79284b47affd02b48eaf
JISOO:)👩‍💻 <155516153+elin0601@users.noreply.github.com>
2024-07-31 23:08:23 +0900

    [level 2] Title: 시소 짝꿍, Time: 138.79 ms, Memory: 118 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int[] weights) {
+        long answer = 0;
+        Arrays.sort(weights);
+        HashMap<Double,Integer> map = new HashMap<>();
+
+        for(Integer weight:weights){
+            
+            double w = Double.valueOf(weight);
+            
+            if(map.containsKey(w)){
+                answer+=map.get(w);
+            }
+            
+            map.put(w,map.getOrDefault(w,0)+1);
+            map.put(w*4/3,map.getOrDefault(w*4/3,0)+1);
+            map.put(w*1.5,map.getOrDefault(w*1.5,0)+1);
+            map.put(w*2,map.getOrDefault(w*2,0)+1);
+        }
+
+
+        return answer;
+    }
+}
